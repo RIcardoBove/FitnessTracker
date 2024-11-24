e3a918a0424a0d62aa8ec015385bd766a9f36880
Ricardo Bove <ricardobove57@gmail.com>
2024-11-23 21:23:54 -0300

    implements delete ans update function

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ListCalcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ListCalcActivity.kt
@@ -1,27 +1,39 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.fitnesstracker.model.App
 import co.tiagoaguiar.fitnesstracker.model.Calc
+import co.tiagoaguiar.fitnesstracker.model.TbmActivity
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ListCalcActivity : AppCompatActivity() {
+class ListCalcActivity : AppCompatActivity(), OnListClickListener {
+
+    private lateinit var adapter: ListCalcAdapter
+    private lateinit var result: MutableList<Calc>
+
+    private lateinit var rv: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_calc)
 
-        var listCalc = mutableListOf<Calc>()
+        result = mutableListOf()
 
-        val rvListCal: RecyclerView = findViewById(R.id.rv_list_calc)
-        rvListCal.layoutManager = LinearLayoutManager(this)
+        adapter = ListCalcAdapter(result, this)
+
+        rv = findViewById(R.id.rv_list_calc)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
 
         val type =
             intent?.extras?.getString("type") ?: throw IllegalStateException("type not found!")
@@ -32,9 +44,7 @@ class ListCalcActivity : AppCompatActivity() {
             val response = dao.getRegisterByType(type)
 
             runOnUiThread {
-                val adapter = ListCalcAdapter(response)
-                rvListCal.adapter = adapter
-                listCalc.addAll(response)
+                result.addAll(response)
                 adapter.notifyDataSetChanged()
             }
 
@@ -42,10 +52,55 @@ class ListCalcActivity : AppCompatActivity() {
 
     }
 
-    private inner class ListCalcAdapter(private val list: List<Calc?>) :
-        RecyclerView.Adapter<ListCalcViewHolder>() {
+    override fun onClick(id: Int, type: String) {
+        when (type) {
+            "imc" -> {
+                val intent = Intent(this, ImcActivity::class.java)
+                intent.putExtra("updateId", id)
+                startActivity(intent)
+            }
+
+            "tbm" -> {
+                val intent = Intent(this, TbmActivity::class.java)
+                intent.putExtra("updateId", id)
+                startActivity(intent)
+            }
+        }
+        finish()
+    }
+
+    override fun onLongClick(position: Int, calc: Calc) {
+        AlertDialog.Builder(this)
+            .setMessage(getString(R.string.delete_message))
+            .setNegativeButton(android.R.string.cancel) {_, _ ->
+
+            }
+            .setPositiveButton(android.R.string.ok) {_, _ ->
+                    Thread{
+                        val app = application as App
+                        val dao = app.db.calcDao()
+                        val response = dao.delete(calc)
+
+                        if (response > 0){
+                            runOnUiThread {
+                                result.removeAt(position)
+                                adapter.notifyItemRemoved(position)
+                            }
+                        }
+                    }.start()
+            }.create()
+            .show()
+
+    }
+
+    private inner class ListCalcAdapter(
+        private val list: List<Calc?>,
+        private val listener: OnListClickListener
+    ) :
+        RecyclerView.Adapter<ListCalcAdapter.ListCalcViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListCalcViewHolder {
             val view = layoutInflater.inflate(R.layout.list_calc_rv, parent, false)
+
             return ListCalcViewHolder(view)
         }
 
@@ -57,22 +112,34 @@ class ListCalcActivity : AppCompatActivity() {
         override fun getItemCount(): Int {
             return list.size
         }
-    }
 
-    private inner class ListCalcViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private inner class ListCalcViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bind(item: Calc) {
-            val textCalc = itemView.findViewById<TextView>(R.id.res_imc)
+            fun bind(item: Calc) {
+                val textCalc = itemView.findViewById<TextView>(R.id.res_imc)
 
-            val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale("pt", "BR"))
-            val data = sdf.format(item.createdDate)
-            val res = item.res
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale("pt", "BR"))
+                val data = sdf.format(item.createdDate)
+                val res = item.res
 
-            textCalc.text = getString(R.string.list_response, res, data)
+                textCalc.text = getString(R.string.list_response, res, data)
 
+                textCalc.setOnLongClickListener {
+                    listener.onLongClick(adapterPosition, item)
+                    true
+                }
+
+                textCalc.setOnClickListener {
+                    listener.onClick(item.id, item.type)
+                }
+
+            }
         }
-
     }
+
 
 }
+
+
+
 
--- app/src/main/java/co/tiagoaguiar/fitnesstracker/model/CalcDao.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/model/CalcDao.kt
@@ -1,14 +1,22 @@
 package co.tiagoaguiar.fitnesstracker.model
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface CalcDao {
 
     @Insert
-    fun insert(calc: Calc )
+    fun insert(calc: Calc)
+
+    @Delete
+    fun delete(calc: Calc): Int
+
+    @Update
+    fun update(calc: Calc)
 
     @Query("SELECT * FROM Calc WHERE type = :type")
     fun getRegisterByType(type: String): List<Calc>
